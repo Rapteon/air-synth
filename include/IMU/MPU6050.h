66c076f4692ce77e19bbef4bb360f934153dad0f
Rapteon <rapteon@tutanota.com>
2025-03-15 20:36:11 +0000

    Updated code structure to deactivate/activate MPU6050 when rising/falling event is received from Button.

---
+++ include/IMU/MPU6050.h
@@ -0,0 +1,84 @@
+#ifndef MPU6050_H
+#define MPU6050_H
+
+#include "IMU/MPU6050.h"
+#include "Button/Button.h"
+
+#include <iostream>
+#include <mutex>
+#include <atomic>
+
+// TODO: These global defines should be part of
+// a config file. The user of this library shouldn't
+// have to worry about configuring the right registers.
+// Calling setters for configuring these registers
+// based on a struct based by the user would be a great idea.
+#define PWR_MGMT_1 0x6B
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_ZOUT_H 0x3F
+
+#define I2C_BUS "/dev/i2c-1"
+#define ACCEL_SCALE 16384.0
+#define G 9.81
+#define ACCEL_THRESHOLD_POS 5.0
+#define ACCEL_THRESHOLD_NEG -5.0
+#define ACCEL_THRESHOLD_POS_Z 3.0
+#define ACCEL_THRESHOLD_NEG_Z -3.0
+#define INCREMENT_VALUE 5
+#define DECREMENT_VALUE 5
+
+class MPU6050 : public Button::ButtonCallbackInterface
+{
+public:
+    virtual void hasEvent(gpiod_line_event &event) override
+    {
+        // TODO: Should handle rising and falling separately
+        // to set active as true/false based on rising/falling.
+        // if (event.event_type == GPIOD_LINE_EVENT_FALLING_EDGE)
+        // {
+        //     set_active(true);
+        //     while (gpiod_line_get_value(line) == 0)
+        //         this_thread::sleep_for(chrono::milliseconds(50));
+        //     set_active(false);
+        // }
+        if (event.event_type == GPIOD_LINE_EVENT_FALLING_EDGE) {
+            std::cerr << "Handling event: falling\n";
+            active = true;
+        }
+        else if (event.event_type == GPIOD_LINE_EVENT_RISING_EDGE) {
+            std::cerr << "Handling event: rising\n";
+            active = false;
+        }
+        else {
+            std::cerr << "Event type: " << event.event_type << '\n';
+        }
+    }
+    MPU6050(int addr);
+
+    ~MPU6050();
+
+    // TODO convert to private method.
+
+    void worker();
+
+    void start();
+
+    // TODO replace content of this method within hasEvent() inherited method.
+    // void button_interrupt(MPU6050 &sensor, int button_gpio);
+
+private:
+    int file;
+    int address;
+    std::mutex mtx;
+    std::atomic<bool> running;
+    std::atomic<bool> active;
+    int counter_x, counter_y, counter_z;
+    bool threshold_crossed_x, threshold_crossed_y, threshold_crossed_z;
+    double z_offset;
+
+    void calibrate_z_axis();
+    int16_t read_word(int8_t reg);
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include "IMU/MPU6050.h"
+#include "Button/Button.h"
+#include <thread>
+
+#define BUTTON_GPIO1 20
+#define BUTTON_GPIO2 21
+
+int main()
+{
+    const char *GPIO_CHIP{"gpiochip0"};
+    constexpr int LEFT_BTN_OFFSET{20};
+    constexpr int RIGHT_BTN_OFFSET{21};
+    constexpr int MPU6050_ADDR1{0x68};
+    constexpr int MPU6050_ADDR2{0x69};
+
+    MPU6050 leftSensor{MPU6050_ADDR1};
+    Button leftButton{};
+    leftButton.registerCallback(&leftSensor);
+    leftSensor.start();
+    leftButton.start(GPIO_CHIP, LEFT_BTN_OFFSET);
+
+    MPU6050 rightSensor{MPU6050_ADDR2};
+    Button rightButton{};
+    rightButton.registerCallback(&rightSensor);
+    rightSensor.start();
+    rightButton.start(GPIO_CHIP, RIGHT_BTN_OFFSET);
+
+    // TODO: Should be replaced with callback registration code.
+    // std::thread t1 (leftSensor.button_interrupt, ref(leftSensor), BUTTON_GPIO1).detach();
+    // std::thread t2 (rightSensor.button_interrupt, ref(rightSensor), BUTTON_GPIO2).detach();
+
+    // while (true) std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    fprintf(stdout, "Press any key to stop\n");
+    getchar();
+    leftButton.stop();
+    rightButton.stop();
+    return 0;
+}
